d4b912aaaf9bcbc943397483f039f4d2b01a75ed
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-01-28 21:01:23 +0530

    firstCommit1

--- cypress/integration/orangeHRM.js
+++ cypress/integration/orangeHRM.js
@@ -3,26 +3,31 @@
 describe('OrangeHRM Test Suite', function()
 {
 
-it('login to OrangeHRM', function () {
+it('login to orangeHRM', function () {
     cy.visit('https://opensource-demo.orangehrmlive.com/index.php/auth/login')
     cy.get('#txtUsername').click().type('Admin')
     cy.get('#txtPassword').click().type('admin123')
     cy.get('#btnLogin').click()
+
+   //search user  
     cy.get('#menu_admin_viewAdminModule > b').click()
     //cy.get('#menu_admin_UserManagement').click()
     cy.get('#searchSystemUser_userName').click().type('admin')
     //cy.wait(2000)
     cy.get('#searchBtn').click()
-})
+    })
 
-it('logout from OrangeHRM', function () {
+    
+it('logout from orangeHRM', function () {
    cy.get('#welcome').click()
    cy.get('#welcome-menu > :nth-child(1) > :nth-child(3) > a').click()
-})
+    })
 
-it('Verify title of Web page - Negetive Test', function () {
+
+it('Verify title of Web page', function () {
     cy.visit('https://opensource-demo.orangehrmlive.com/index.php/auth/login')
     cy.title() .should('eq' , 'OrangeHRM')
-})
+    })
+
 
 })
