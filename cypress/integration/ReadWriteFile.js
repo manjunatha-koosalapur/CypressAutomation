18d1677ccb59c14043bd99d7695c6daa221da2a4
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-01-31 22:51:19 +0530

    cmt9

--- cypress/integration/ReadWriteFile.js
+++ cypress/integration/ReadWriteFile.js
@@ -8,4 +8,4 @@ cy.writeFile('testFile1.txt', 'Test automation by Cypress.io')
 it ('Read from file', function(){
     cy.readFile('testFile1.txt').should('contains','Test automation by Cypress.io')
     
-    })
+})
